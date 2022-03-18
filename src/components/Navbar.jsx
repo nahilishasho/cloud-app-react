92db0d596ea6c0d97af207aab9c630970a887522
@nahilishasho <shashonahili702@gmail.com>
2022-03-18 12:46:06 +0300

    change name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () =>{
     <div className = 'w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
         <div className = 'px-2 flex justify-between items-center w-full h-full'>
             <div className = 'flex items-center'>
-                <h1 className = 'text-3x1 font-bold mr-4 sm:text-4xl'>BRAND.</h1>
+                <h1 className = 'text-3x1 font-bold mr-4 sm:text-4xl'>BRANDINGo.</h1>
             </div>
         </div>
         <MenuIcon className='w-5'/>
